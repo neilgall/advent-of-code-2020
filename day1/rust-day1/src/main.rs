d8d20a8eda6dad7415311dff844bf897b5ab0a8a
Neil Gall <neil@gall.scot>
2020-12-01 15:09:51 +0000

    Convert to spaces

--- day1/rust-day1/src/main.rs
+++ day1/rust-day1/src/main.rs
@@ -4,37 +4,37 @@ use std::io::prelude::*;
 
 
 fn read_file(filename: &str) -> std::io::Result<Vec<i64>> {
-	let mut file = File::open(filename)?;
-	let mut contents = String::new();
-	file.read_to_string(&mut contents)?;
-	let input = contents.lines().filter_map(|line| line.parse().ok()).collect();
-	Ok(input)
+    let mut file = File::open(filename)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let input = contents.lines().filter_map(|line| line.parse().ok()).collect();
+    Ok(input)
 }
 
 fn part1(input: &Vec<i64>) -> i64 {
-	input.iter().flat_map(
-		move |x| input.iter().filter_map(
-			move |y|
-				if x+y == 2020 { Some(x*y) } else { None }
-		)
-	).next().unwrap()
+    input.iter().flat_map(
+        move |x| input.iter().filter_map(
+            move |y|
+                if x+y == 2020 { Some(x*y) } else { None }
+        )
+    ).next().unwrap()
 }
 
 fn part2(input: &Vec<i64>) -> i64 {
-	input.iter().flat_map(
-		move |x| input.iter().flat_map(
-			move |y| input.iter().filter_map(
-				move |z| 
-				if x+y+z == 2020 { Some(x*y*z) } else { None }
-			)
-		)
-	).next().unwrap()
+    input.iter().flat_map(
+        move |x| input.iter().flat_map(
+            move |y| input.iter().filter_map(
+                move |z| 
+                    if x+y+z == 2020 { Some(x*y*z) } else { None }
+            )
+        )
+    ).next().unwrap()
 
 }
 
 
 fn main() {
-	let input = read_file("../input.txt").unwrap();
+    let input = read_file("../input.txt").unwrap();
     println!("Part1 {}", part1(&input));
     println!("Part2 {}", part2(&input));
 }
