a159af5b7a44237c3824b3e17fd764cdf8cf8077
Neil Gall <neil@gall.scot>
2020-12-01 12:40:04 +0000

    Fix gitignore

---
+++ day1/day1.hs
@@ -0,0 +1,33 @@
+
+load :: String -> [Int]
+load = map read . lines
+
+listWhichAddTo2020 :: [[Int]] -> [Int]
+listWhichAddTo2020 = head . filter addsTo2020
+
+addsTo2020 :: [Int] -> Bool
+addsTo2020 xs = sum xs == 2020
+
+part1 :: [Int] -> Int
+part1 input = product $ listWhichAddTo2020 sums
+  where
+    sums = do
+      x <- input
+      y <- input
+      return [x, y]
+
+part2 :: [Int] -> Int
+part2 input = product $ listWhichAddTo2020 sums
+  where
+    sums = do
+      x <- input
+      y <- input
+      z <- input
+      return [x, y, z]
+
+
+main = do
+  input <- fmap load (readFile "input.txt")
+  putStrLn $ show $ part1 input
+  putStrLn $ show $ part2 input
+
