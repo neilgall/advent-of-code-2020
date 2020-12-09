90e5e4edec16aebf69fb1072283696c4c05716f1
Neil Gall <neil@gall.scot>
2020-12-09 07:50:37 +0000

    Day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -33,13 +33,44 @@ fn find_first_invalid(vec: &Vec<i64>, preamble: usize) -> Option<i64> {
     index.map(|i| vec[i])
 }
 
+fn find_contiguous_set_summing_to(target: i64, vec: &[i64]) -> Option<&[i64]> {
+    let mut start = 0;
+    let mut end = 1;
+    let len = vec.len();
+
+    while end < len {
+        let sum: i64 = vec[start..=end].iter().sum();
+        if sum == target {
+            return Some(&vec[start..=end]);
+        }
+        if sum > target {
+            start += 1;
+        } else {
+            end += 1;
+        }
+    }
+
+    None
+}
+
+fn sum_of_min_and_max(vec: &[i64]) -> Option<i64> {
+    vec.iter().min()
+        .and_then(|min|
+            vec.iter().max().map(|max| 
+                min + max
+            )
+        )
+}
+
 fn part1(sequence: &Vec<i64>) -> Option<i64> {
     find_first_invalid(sequence, 25)
 }
 
-fn part2(sequence: &Vec<i64>) -> i64 {
-    0
-}
+fn part2(sequence: &Vec<i64>) -> Option<i64> {
+    let target = part1(sequence).unwrap();
+    find_contiguous_set_summing_to(target, sequence)
+        .and_then(sum_of_min_and_max)
+}   
 
 
 fn main() {
@@ -69,8 +100,21 @@ mod tests {
     }
 
     #[test]
-    fn test_xmas_decoder() {
+    fn test_find_first_invalid() {
         let input = vec![35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576];
         assert_eq!(find_first_invalid(&input, 5), Some(127));
+    }
+
+    #[test]
+    fn test_find_contiguous_set_summing_to() {
+        let input = vec![35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576];
+        let expect: &[i64] = &vec![15,25,47,40];
+        assert_eq!(find_contiguous_set_summing_to(127, &input), Some(expect));     
+    }
+
+    #[test]
+    fn test_sum_of_min_and_max() {
+        let input = vec![15,25,47,40];
+        assert_eq!(sum_of_min_and_max(&input), Some(62));
     }
 }
