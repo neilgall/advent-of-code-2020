2d47beeb959f8e293bafef48bfd74c7c5448206f
Neil Gall <neil@gall.scot>
2020-12-13 08:55:56 +0000

    Day 13 part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -18,7 +18,7 @@ type BusID = i64;
 
 struct Input {
     estimate: Timestamp,
-    in_service_buses: Vec<BusID>
+    bus_ids: Vec<Option<BusID>>
 }
 
 impl From<&str> for Input {
@@ -28,12 +28,11 @@ impl From<&str> for Input {
             integer.map(Option::Some)
         );
 
-        let valid_bus_ids = bus_id.sep_by(match_literal(","))
-            .map(|ids| ids.iter().filter_map(|id| *id).collect());
+        let bus_ids = bus_id.sep_by(match_literal(","));
 
 
-        let input = pair(whitespace_wrap(integer), valid_bus_ids,
-            |estimate, in_service_buses| Input { estimate, in_service_buses }
+        let input = pair(whitespace_wrap(integer), bus_ids,
+            |estimate, bus_ids| Input { estimate, bus_ids }
         );
 
         input.parse(s).unwrap().1
@@ -42,23 +41,62 @@ impl From<&str> for Input {
 
 // --- problems
 
-fn next_bus_departing(input: &Input) -> Option<(BusID, Timestamp)> {
-    let buses_with_wait_times: Vec<(BusID, Timestamp)> = input.in_service_buses.iter()
-        .map(|id| (*id, id - (input.estimate % id)))
-        .collect();
+impl Input {
+    fn next_bus_departing(&self) -> Option<(BusID, Timestamp)> {
+        let buses_with_wait_times: Vec<(BusID, Timestamp)> = self.bus_ids.iter()
+            .filter_map(|maybe_id| *maybe_id)
+            .map(|id| (id, id - (self.estimate % id)))
+            .collect();
 
-    buses_with_wait_times.iter().min_by_key(|(_id, wait_time)| wait_time)
-        .map(|r| r.clone())
+        buses_with_wait_times.iter().min_by_key(|(_id, wait_time)| wait_time)
+            .map(|r| r.clone())
+    }
+
+    fn buses_align_at_timestamp(&self, timestamp: Timestamp) -> bool {
+        self.bus_ids.iter()
+            .enumerate()
+            .all(|(index,maybe_id)| 
+                match maybe_id {
+                    Some(id) => (timestamp + index as Timestamp) % id == 0,
+                    None => true
+                }
+            )
+    }
+
+    fn find_first_aligned_timestamp(&self, after: Timestamp) -> Timestamp {
+        let indexed_bus_ids: Vec<(usize, BusID)> = self.bus_ids.iter()
+            .enumerate()
+            .filter_map(|(index, maybe_id)| maybe_id.map(|id| (index, id)))
+            .rev()
+            .collect();
+
+        indexed_bus_ids.iter().fold( (after, 1), |(base_timestamp, increment), (index, bus_id)| {
+            let index = *index as Timestamp;
+            (0..).find_map(|i| {
+                let timestamp = base_timestamp + i * increment;
+                if (timestamp + index) % bus_id == 0 {
+                    Some( (timestamp, increment * bus_id) )
+                } else {
+                    None
+                }
+            }).unwrap()
+        }).0
+    }
 }
 
 fn part1(input: &Input) -> Option<i64> {
-    next_bus_departing(input).map(|(id, wait)| id * wait)
+    input.next_bus_departing().map(|(id, wait)| id * wait)
+}
+
+fn part2(input: &Input) -> Timestamp {
+    input.find_first_aligned_timestamp(100000000000000)
 }
 
 
 fn main() {
     let input = Input::from(read_file("./input.txt").unwrap().as_str());
     println!("part1 {:?}", part1(&input));
+    println!("part2 {:?}", part2(&input));
 }
 
 #[cfg(test)]
@@ -69,12 +107,56 @@ mod tests {
     fn test_parser() {
         let input = Input::from("939\n7,13,x,x,59,x,31,19");
         assert_eq!(input.estimate, 939);
-        assert_eq!(input.in_service_buses, vec![7,13,59,31,19]);
+        assert_eq!(input.bus_ids, vec![Some(7),Some(13),None,None,Some(59),None,Some(31),Some(19)]);
     }
 
     #[test]
     fn test_next_bus_departing() {
         let input = Input::from("939\n7,13,x,x,59,x,31,19");
-        assert_eq!(next_bus_departing(&input), Some((59, 5)));        
+        assert_eq!(input.next_bus_departing(), Some((59, 5)));        
+    }
+
+    #[test]
+    fn test_buses_align_at_timestamp() {
+        let input = Input::from("939\n7,13,x,x,59,x,31,19");
+        assert!(input.buses_align_at_timestamp(1068781));        
+        assert!(!input.buses_align_at_timestamp(1068780));        
+        assert!(!input.buses_align_at_timestamp(1068782));        
+    }
+
+    #[test]
+    fn test_find_first_aligned_timestamp_1() {
+        let input = Input::from("939\n7,13,x,x,59,x,31,19");
+        assert_eq!(input.find_first_aligned_timestamp(1000000), 1068781);        
+    }
+
+    #[test]
+    fn test_find_first_aligned_timestamp_2() {
+        let input = Input::from("0\n17,x,13,19");
+        assert_eq!(input.find_first_aligned_timestamp(0), 3417);        
+    }
+
+    #[test]
+    fn test_find_first_aligned_timestamp_3() {
+        let input = Input::from("0\n67,7,59,61");
+        assert_eq!(input.find_first_aligned_timestamp(0), 754018);        
+    }
+
+    #[test]
+    fn test_find_first_aligned_timestamp_4() {
+        let input = Input::from("0\n67,x,7,59,61");
+        assert_eq!(input.find_first_aligned_timestamp(0), 779210);        
+    }
+
+    #[test]
+    fn test_find_first_aligned_timestamp_5() {
+        let input = Input::from("0\n67,7,x,59,61");
+        assert_eq!(input.find_first_aligned_timestamp(0), 1261476);        
+    }
+
+    #[test]
+    fn test_find_first_aligned_timestamp_6() {
+        let input = Input::from("0\n1789,37,47,1889");
+        assert_eq!(input.find_first_aligned_timestamp(0), 1202161486);        
     }
 }
