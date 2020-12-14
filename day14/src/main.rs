d18eda0ae40f7c45c4ecb01305481d548331743a
Neil Gall <neil@gall.scot>
2020-12-14 08:38:31 +0000

    Day 14 part 2

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -17,15 +17,9 @@ fn read_file(filename: &str) -> std::io::Result<String> {
 type Address = u64;
 type Word = u64;
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-struct WriteMask {
-    zeros: Word,
-    ones: Word
-}
-
 #[derive(Debug, Eq, PartialEq)]
 enum Instruction {
-    Mask(WriteMask),
+    Mask { zeros: Word, ones: Word },
     Write { address: Address, value: Word }
 }
 
@@ -33,35 +27,68 @@ type Program = Vec<Instruction>;
 
 struct Machine {
     memory: HashMap<Address, Word>,
-    mask: WriteMask
+    mask_zeros: Word,
+    mask_ones: Word,
+    mask_floating: Word
 }
 
 impl Machine {
     fn new() -> Self {
         Machine { 
             memory: HashMap::new(),
-            mask: WriteMask { zeros: 0, ones: 0 }
+            mask_zeros: 0,
+            mask_ones: 0,
+            mask_floating: 0
         }
     }
 
     fn run(&mut self, program: &Program) {
         program.iter().for_each(|instruction| match instruction {
 
-            Instruction::Mask(mask) => {
-                self.mask = *mask;
+            Instruction::Mask { zeros, ones } => {
+                self.mask_zeros = *zeros;
+                self.mask_ones = *ones;
             }
 
             Instruction::Write { address, value } => {
-                self.memory.insert(*address, value & (!self.mask.zeros) | self.mask.ones);
+                self.memory.insert(*address, value & (!self.mask_zeros) | self.mask_ones);
             }
         });
+    }
+
+    fn run_v2(&mut self, program: &Program) {
+        program.iter().for_each(|instruction| match instruction {
+
+            Instruction::Mask { zeros, ones } => {
+                self.mask_zeros = *zeros;
+                self.mask_ones = *ones;
+                self.mask_floating = !(zeros | ones) & 0xfffffffff;
+            }
+
+            Instruction::Write { address, value } => {
+                let address = address & !(self.mask_floating) | self.mask_ones;
+                self.write_floating_address(&address, value, 0);
+            }
+        });
+    }
+
+    fn write_floating_address(&mut self, address: &Address, value: &Word, bit_index: usize) {
+        let bit_mask = 1 << bit_index;
+        if self.mask_floating & bit_mask != 0 {
+            [address & !bit_mask, address | bit_mask].iter().for_each(|address| {
+                self.memory.insert(*address, *value);
+                self.write_floating_address(address, value, bit_index + 1);
+            });
+
+        } else if self.mask_floating >> bit_index != 0 {
+            self.write_floating_address(address, value, bit_index + 1)
+        }
     }
 
     fn sum_of_all_memory_words(&self) -> Word {
         self.memory.values().sum()
     }
 }
-
 
 // -- parser
 
@@ -87,7 +114,7 @@ fn parse_input(input: &str) -> ParseResult<Program> {
                     MaskBit::Unchanged => (zeros, ones),
                 }
             );
-            Instruction::Mask(WriteMask { zeros, ones })
+            Instruction::Mask { zeros, ones }
         });
 
     let write = pair(
@@ -112,13 +139,19 @@ fn part1(program: &Program) -> Word {
     let mut machine = Machine::new();
     machine.run(&program);
     machine.sum_of_all_memory_words()
+}
 
+fn part2(program: &Program) -> Word {
+    let mut machine = Machine::new();
+    machine.run_v2(&program);
+    machine.sum_of_all_memory_words()
 }
 
 fn main() {
     let input = read_file("./input.txt").unwrap();
     let program = parse_input(&input).unwrap().1;
     println!("part 1 {:?}", part1(&program));
+    println!("part 2 {:?}", part2(&program));
 }
 
 #[cfg(test)]
@@ -136,7 +169,7 @@ mod tests {
     fn test_parser() {
         let program = parse_input(sample_program());
         assert_eq!(program, Ok(("", vec![
-            Instruction::Mask(WriteMask { zeros: 2, ones: 64 }),
+            Instruction::Mask { zeros: 2, ones: 64 },
             Instruction::Write { address: 8, value: 11 },
             Instruction::Write { address: 7, value: 101 },
             Instruction::Write { address: 8, value: 0 }
@@ -147,5 +180,16 @@ mod tests {
     fn test_part1() {
         let program = parse_input(sample_program()).unwrap().1;
         assert_eq!(part1(&program), 165);
+    }
+
+    #[test]
+    fn test_part2() {
+        let program = parse_input("
+            mask = 000000000000000000000000000000X1001X
+            mem[42] = 100
+            mask = 00000000000000000000000000000000X0XX
+            mem[26] = 1
+        ").unwrap().1;
+        assert_eq!(part2(&program), 208);
     }
 }
