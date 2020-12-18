51a8b8690c777f914a0b34568f6db26cc4637080
Neil Gall <neil@gall.scot>
2020-12-18 09:18:50 +0000

    Day 18 part 2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -21,7 +21,10 @@ fn tokenize(input: &str) -> ParseResult<Vec<Token>> {
     one_or_more(token).parse(input)
 }
 
-fn shunting_yard(tokens: &[Token]) -> Vec<&Token> {
+fn shunting_yard<F>(tokens: &[Token], precedence: F) -> Vec<&Token>
+where
+    F: Fn(&Token, &Token) -> bool
+{
     let mut stack: Vec<&Token> = vec![];
     let mut result: Vec<&Token> = vec![];
 
@@ -33,7 +36,7 @@ fn shunting_yard(tokens: &[Token]) -> Vec<&Token> {
 
             Token::Add | Token::Mul => {
                 while let Some(t) = stack.last() {
-                    if t == &&Token::Add || t == &&Token::Mul {
+                    if precedence(token, *t) {
                         result.push(*t);
                         stack.pop();
                     } else {
@@ -66,6 +69,14 @@ fn shunting_yard(tokens: &[Token]) -> Vec<&Token> {
     result
 }
 
+fn shunting_yard_v1(tokens: &[Token]) -> Vec<&Token> {
+    shunting_yard(tokens, |_, t| t == &Token::Add || t == &Token::Mul)
+}
+
+fn shunting_yard_v2(tokens: &[Token]) -> Vec<&Token> {
+    shunting_yard(tokens, |t1, t2| (t2 == &Token::Add || t2 == &Token::Mul) && !(t2 == &Token::Mul && t1 == &Token::Add))
+}
+
 fn eval_rp(tokens: &[&Token]) -> i64 {
     let mut stack: Vec<i64> = vec![];
 
@@ -94,19 +105,30 @@ fn eval_rp(tokens: &[&Token]) -> i64 {
     stack.pop().unwrap()
 }
 
-fn eval(input: &str) -> i64 {
+fn eval_v1(input: &str) -> i64 {
     let tokens = tokenize(input).unwrap().1;
-    let rp = shunting_yard(&tokens);
+    let rp = shunting_yard_v1(&tokens);
+    eval_rp(&rp)
+}
+
+fn eval_v2(input: &str) -> i64 {
+    let tokens = tokenize(input).unwrap().1;
+    let rp = shunting_yard_v2(&tokens);
     eval_rp(&rp)
 }
 
 fn part1(input: &str) -> i64 {
-    input.lines().map(eval).sum()
+    input.lines().map(eval_v1).sum()
+}
+
+fn part2(input: &str) -> i64 {
+    input.lines().map(eval_v2).sum()
 }
 
 fn main() {
     let input = std::fs::read_to_string("./input.txt").unwrap();
     println!("part 1 {}", part1(&input));
+    println!("part 2 {}", part2(&input));
 }
 
 #[cfg(test)]
@@ -122,17 +144,17 @@ mod tests {
     }
 
     #[test]
-    fn test_shunting_yard_simple_add() {
+    fn test_shunting_yard_v1_simple_add() {
         use Token::*;
         let input = [Num(1), Add, Num(2)];
-        assert_eq!(shunting_yard(&input), vec![&Num(1), &Num(2), &Add])
+        assert_eq!(shunting_yard_v1(&input), vec![&Num(1), &Num(2), &Add])
     }
 
     #[test]
-    fn test_shunting_yard_with_parens() {
+    fn test_shunting_yard_v1_with_parens() {
         use Token::*;
         let input = [Num(1), Add, Open, Num(2), Mul, Num(3), Close, Add, Num(7)];
-        assert_eq!(shunting_yard(&input), vec![&Num(1), &Num(2), &Num(3), &Mul, &Add, &Num(7), &Add])
+        assert_eq!(shunting_yard_v1(&input), vec![&Num(1), &Num(2), &Num(3), &Mul, &Add, &Num(7), &Add])
     }
 
     #[test]
@@ -142,10 +164,19 @@ mod tests {
     }
 
     #[test]
-    fn test_eval() {
-        assert_eq!(eval("2 * 3 + (4 * 5)"), 26);
-        assert_eq!(eval("5 + (8 * 3 + 9 + 3 * 4 * 3)"), 437);
-        assert_eq!(eval("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"), 12240);
-        assert_eq!(eval("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"), 13632);
+    fn test_eval_v1() {
+        assert_eq!(eval_v1("2 * 3 + (4 * 5)"), 26);
+        assert_eq!(eval_v1("5 + (8 * 3 + 9 + 3 * 4 * 3)"), 437);
+        assert_eq!(eval_v1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"), 12240);
+        assert_eq!(eval_v1("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"), 13632);
+    }
+
+    #[test]
+    fn test_eval_v2() {
+        assert_eq!(eval_v2("1 + (2 * 3) + (4 * (5 + 6))"), 51);
+        assert_eq!(eval_v2("2 * 3 + (4 * 5)"), 46);
+        assert_eq!(eval_v2("5 + (8 * 3 + 9 + 3 * 4 * 3)"), 1445);
+        assert_eq!(eval_v2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"), 669060);
+        assert_eq!(eval_v2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"), 23340);
     }
 }
