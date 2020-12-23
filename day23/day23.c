e847450eb04befde3830d90d19de3592d70817a1
Neil Gall <neil@gall.scot>
2020-12-23 18:16:02 +0000

    Revert to C for linked list solution to part 2

---
+++ day23/day23.c
@@ -0,0 +1,164 @@
+#include <assert.h>
+#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned long cup_id;
+
+struct cup {
+	cup_id id;
+	struct cup *next;
+};
+
+struct cups {
+	struct cup *current;
+	struct cup **by_id;
+	size_t length;
+};
+
+void print_cups(const struct cups *cups) {
+	const struct cup *cup = cups->by_id[1];
+	do {
+		if (cup == cups->current)
+			printf("(%lu) ", cup->id);
+		else 
+			printf("%lu ", cup->id);
+		cup = cup->next;
+	} while (cup != cups->by_id[1]);
+	printf("\n");
+}
+
+const struct cup *first_after_1(const struct cups *cups) {
+	return cups->by_id[1]->next;
+}
+
+cup_id prev(cup_id id, size_t length) {
+	return id == 1 ? length : id - 1;
+}
+
+void apply_move(struct cups *cups) {
+	struct cup *curr = cups->current;
+	struct cup *next1 = curr->next;
+	struct cup *next2 = next1->next;
+	struct cup *next3 = next2->next;
+	struct cup *next4 = next3->next;
+
+	cup_id dest_id = prev(cups->current->id, cups->length);
+	struct cup *dest;
+	do {
+		dest = cups->by_id[dest_id];
+		dest_id = prev(dest_id, cups->length);
+	}
+	while (dest == next1 || dest == next2 || dest == next3);
+
+	struct cup *dest_next1 = dest->next;
+	struct cup *dest_next2 = dest_next1->next;
+	struct cup *dest_next3 = dest_next2->next;
+
+	// remove next1..next3 from ring
+	curr->next = next4;
+
+	// reinsert after dest
+	dest->next = next1;
+	next3->next = dest_next1;
+
+	cups->current = cups->current->next;
+}
+
+void apply_n_moves(struct cups *cups, size_t count) {
+	for (size_t i = 0; i < count; ++i) {
+		apply_move(cups);
+	}
+}
+
+struct cup *new_cup(cup_id id) {
+	struct cup* cup = (struct cup *)malloc(sizeof(struct cup));
+	cup->id = id;
+	cup->next = NULL;
+	return cup;
+}
+
+struct cups *make_cups(const char *init, size_t length, cup_id current) {
+	struct cups *cups = (struct cups *)malloc(sizeof(struct cups));
+	cups->current = NULL;
+	cups->length = length;
+	cups->by_id = (struct cup **)malloc(sizeof(struct cup *) * (length + 1));
+
+	struct cup *first = NULL;
+	struct cup **next_p = &first;
+	size_t count = 1;
+	
+	while (*init) {
+		struct cup *cup = new_cup((*init++) - '0');
+		*next_p = cup;
+		next_p = &cup->next;
+		cups->by_id[cup->id] = cup;
+		count++;
+	}
+
+	while (count <= length) {
+		struct cup *cup = new_cup(count++);
+		*next_p = cup;
+		next_p = &cup->next;
+		cups->by_id[cup->id] = cup;
+	}
+
+	*next_p = first;
+	cups->current = cups->by_id[current];
+
+	return cups;
+}
+
+void assert_cups(const char *tag, const struct cups *cups, const char *expect) {
+	for (const struct cup *cup = first_after_1(cups); *expect; cup = cup->next, expect++) {
+		cup_id expected_cup = *expect - '0';
+		if (cup->id != expected_cup) {
+			printf("%s: expected %u got %u\n", tag, expected_cup, cup->id);
+		}
+	}
+}
+
+void test_10_moves() {
+	struct cups *cups = make_cups("389125467", 9, 3);
+	apply_n_moves(cups, 10);
+	assert_cups("test 10 moves", cups, "92658374");
+}
+
+
+void test_100_moves() {
+	struct cups *cups = make_cups("389125467", 9, 3);
+	apply_n_moves(cups, 100);
+	assert_cups("test 100 moves", cups, "67384529");
+}
+
+void test_10_million_moves() {
+	struct cups *cups = make_cups("389125467", 1000000, 3);
+	apply_n_moves(cups, 10000000);
+	const struct cup *cup = first_after_1(cups);
+	cup_id prod = cup->id * cup->next->id;
+	if (prod != 149245887792) {
+		printf("test 10 million moves: expected 149245887792 got %lu * %lu = %lu\n", cup->id, cup->next->id, prod);
+	}
+}
+
+void run_tests() {
+	test_10_moves();
+	test_100_moves();
+	test_10_million_moves();
+}
+
+cup_id part2(struct cups *cups) {
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	if (argc > 1 && strcmp(argv[1], "test") == 0) {
+		run_tests();
+	} else {
+		struct cups* cups = make_cups("523764819", 1000000, 5);
+		apply_n_moves(cups, 10000000);
+		const struct cup *cup = first_after_1(cups);
+		printf("part 2: %lu\n", cup->id * cup->next->id);
+	}
+}
--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,4 +1,6 @@
+use std::io::Write;
 use std::iter::once;
+use std::time::{Duration, SystemTime};
 
 // -- model
 
@@ -6,8 +8,9 @@ type Cup = u32;
 
 #[derive(Debug)]
 struct Cups {
-    cups: Vec<Cup>,
-    offset: usize
+    length: usize,
+    offset: usize,
+    cups: Vec<Cup>
 }
 
 #[derive(Debug)]
@@ -20,14 +23,32 @@ fn prev(cup: Cup) -> Cup {
     if cup == 1 { 9 } else { cup - 1 }
 }
 
+fn str_as_cups(s: &str) -> impl Iterator<Item = Cup> + '_ {
+    s.chars().map(|c| (c as Cup) - 48)
+}
+
+fn estimate_remaining(done: usize, total: usize, start: &SystemTime) -> Duration {
+    if done == 0 {
+        Duration::from_secs(0)
+    } else {
+        start.elapsed().unwrap() * (total as u32) / (done as u32)
+    }
+}
+
 impl Cups {
-    fn new(input: &str, current_cup: Cup) -> Self {
-        let cups: Vec<Cup> = input.chars().map(|c| (c as Cup) - 48).collect();
+    fn new<I>(input: I, current_cup: Cup) -> Self where I: Iterator<Item = Cup> {
+        let cups: Vec<Cup> = input.collect();
         let offset = cups.iter().position(|c| *c == current_cup).unwrap();
+        let length = cups.len();
         Cups {
             cups,
+            length,
             offset
         }
+    }
+
+    fn from_str(input: &str, current_cup: Cup) -> Self {
+        Cups::new(str_as_cups(input), current_cup)
     }
 
     fn current_cup(&self) -> Cup {
@@ -43,8 +64,8 @@ impl Cups {
     }
 
     fn labels(&self) -> Vec<Cup> {
-        let start = (self.index_of_cup(1) + 1) % 9;
-        self.cups.iter().cycle().skip(start).take(8).copied().collect()
+        let start = (self.index_of_cup(1) + 1) % self.length;
+        self.cups.iter().cycle().skip(start).take(self.length-1).copied().collect()
     }
 
     fn labels_as_str(&self) -> String {
@@ -66,39 +87,35 @@ impl Cups {
     fn apply(&mut self, m: Move) {
         let dest_index = self.index_of_cup(m.destination);
 
-
         let remaining_cups = self.cups.iter()
             .cycle()
             .skip(dest_index+1)
             .filter(|c| !m.removed.contains(c))
-            .take(5)
-            .copied();
+            .take(self.length - 4);
 
-        let new_cups: Vec<Cup> = once(m.destination)
-            .chain(m.removed.iter().copied())
+        let new_cups: Vec<Cup> = once(&m.destination)
+            .chain(m.removed.iter())
             .chain(remaining_cups)
+            .copied()
             .collect();
 
         self.cups = new_cups;
 
-        let diff = (dest_index + 9 - self.offset) % 9;
+        let diff = (dest_index + self.length - self.offset) % self.length;
+        let new_offset = if diff <= 4 { 0 } else { self.length - diff + 4 };
 
-        let new_offset = match diff {
-            4 => 0,
-            5 => 8,
-            6 => 7,
-            7 => 6,
-            8 => 5,
-            _ => panic!(format!("unexpected diff {}", diff))
-        };
-
-        println!("offset={} dest={} diff={} cups={:?} new_offset={}", self.offset, dest_index, diff, self.cups, new_offset);
+        // println!("offset={} dest={} diff={} cups={:?} new_offset={}", self.offset, dest_index, diff, self.cups, new_offset);
         self.offset = new_offset;
     }
 
     fn apply_n_moves(&mut self, count: usize) {
-        for _ in 0..count {
+        let start = SystemTime::now();
+        for n in 0..count {
             self.apply(self.create_move());
+            if n % 1000 == 0 {
+                print!("{}... {:?} remaining\r", n, estimate_remaining(n, count, &start));
+                std::io::stdout().flush().unwrap();
+            }
         }
     }
 }
@@ -106,14 +123,23 @@ impl Cups {
 // -- problems
 
 fn part1(input: &str, start_cup: Cup) -> String {
-    let mut cups = Cups::new(input, start_cup);
+    let mut cups = Cups::from_str(input, start_cup);
     cups.apply_n_moves(100);
     cups.labels_as_str()
+}
+
+fn part2(input: &str, start_cup: Cup) -> Cup {
+    let mut cups = Cups::new(str_as_cups(input).chain(10..=1_000_000), start_cup);
+    cups.apply_n_moves(10_000_000);
+
+    let first_2: Vec<Cup> = cups.labels().iter().take(2).copied().collect();
+    first_2[0] * first_2[1]
 }
 
 fn main() {
     let input = "523764819";
     println!("part 1 {}", part1(input, 5));
+    println!("part 2 {}", part2(input, 5));
 }
 
 #[cfg(test)]
@@ -122,21 +148,21 @@ mod tests {
 
     #[test]
     fn test_iter_current_3() {
-        let cups = Cups::new("389125467", 3);
+        let cups = Cups::from_str("389125467", 3);
         let x: Vec<Cup> = cups.iter().take(15).collect();
         assert_eq!(x, vec![3, 8, 9, 1, 2, 5, 4, 6, 7, 3, 8, 9, 1, 2, 5]);
     }
 
     #[test]
     fn test_iter_current_2() {
-        let cups = Cups::new("389125467", 2);
+        let cups = Cups::from_str("389125467", 2);
         let x: Vec<Cup> = cups.iter().take(15).collect();
         assert_eq!(x, vec![2, 5, 4, 6, 7, 3, 8, 9, 1, 2, 5, 4 ,6, 7, 3]);
     }
 
     #[test]
     fn test_create_move_1() {
-        let cups = Cups::new("389125467", 3);
+        let cups = Cups::from_str("389125467", 3);
         let mv = cups.create_move();
         assert_eq!(mv.removed, vec![8, 9, 1]);
         assert_eq!(mv.destination, 2);
@@ -144,7 +170,7 @@ mod tests {
 
     #[test]
     fn test_create_move_3() {
-        let cups = Cups::new("325467891", 5);
+        let cups = Cups::from_str("325467891", 5);
         let mv = cups.create_move();
         assert_eq!(mv.removed, vec![4, 6, 7]);
         assert_eq!(mv.destination, 3);
@@ -152,7 +178,7 @@ mod tests {
 
     #[test]
     fn test_apply_move_1() {
-        let mut cups = Cups::new("389125467", 3);
+        let mut cups = Cups::from_str("389125467", 3);
         cups.apply(cups.create_move());
         assert_eq!(cups.labels(), vec![5, 4, 6, 7, 3, 2, 8, 9]);
         assert_eq!(cups.current_cup(), 2);
@@ -160,7 +186,7 @@ mod tests {
 
     #[test]
     fn test_apply_move_2() {
-        let mut cups = Cups::new("328915467", 2);
+        let mut cups = Cups::from_str("328915467", 2);
         println!("move {:?}", cups.create_move());
         cups.apply(cups.create_move());
         assert_eq!(cups.labels(), vec![3, 2, 5, 4, 6, 7, 8, 9]);
@@ -169,7 +195,7 @@ mod tests {
 
     #[test]
     fn test_apply_move_4() {
-        let mut cups = Cups::new("725891346", 8);
+        let mut cups = Cups::from_str("725891346", 8);
         cups.apply(cups.create_move());
         assert_eq!(cups.labels(), vec![3, 2, 5, 8, 4, 6,7, 9]);
         assert_eq!(cups.current_cup(), 4);
@@ -177,7 +203,7 @@ mod tests {
 
     #[test]
     fn test_apply_move_5() {
-        let mut cups = Cups::new("325846791", 4);
+        let mut cups = Cups::from_str("325846791", 4);
         cups.apply(cups.create_move());
         assert_eq!(cups.labels(), vec![3, 6, 7, 9, 2, 5, 8, 4]);
         assert_eq!(cups.current_cup(), 1);
@@ -185,7 +211,7 @@ mod tests {
 
     #[test]
     fn test_apply_move_9() {
-        let mut cups = Cups::new("741583926", 6);
+        let mut cups = Cups::from_str("741583926", 6);
         let mv = cups.create_move();
         assert_eq!(mv.removed, vec![7, 4, 1]);
         assert_eq!(mv.destination, 5);
@@ -196,14 +222,14 @@ mod tests {
 
     #[test]
     fn test_10_moves() {
-        let mut cups = Cups::new("389125467", 3);
+        let mut cups = Cups::from_str("389125467", 3);
         cups.apply_n_moves(10);
         assert_eq!(cups.labels(), vec![9, 2, 6, 5, 8, 3, 7, 4]);
     }
 
     #[test]
     fn test_100_moves() {
-        let mut cups = Cups::new("389125467", 3);
+        let mut cups = Cups::from_str("389125467", 3);
         cups.apply_n_moves(100);
         assert_eq!(cups.labels(), vec![6, 7, 3, 8, 4, 5, 2, 9]);        
     }
