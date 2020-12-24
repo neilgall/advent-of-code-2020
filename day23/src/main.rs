2239649f70e9f350312404e73d14684132cf4650
Neil Gall <neil@gall.scot>
2020-12-24 08:56:33 +0000

    Day 23 in Rust without unsafe!

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,42 +1,30 @@
-use std::ptr;
 
 // -- model
 
-type CupID = usize;
-
-#[derive(Debug)]
-struct Cup {
-    id: CupID,
-    next: *mut Cup
-}
+type Cup = usize;
 
 #[derive(Debug)]
 struct Cups {
     length: usize,
     cups: Vec<Cup>,
-    current: *mut Cup
+    current: Cup
 }
 
-fn str_as_cup_ids(s: &str) -> impl Iterator<Item = CupID> + '_ {
-    s.chars().map(|c| (c as CupID) - 48)
+fn str_as_cup_ids(s: &str) -> impl Iterator<Item = Cup> + '_ {
+    s.chars().map(|c| (c as Cup) - 48)
 }
 
 impl Cups {
-    fn new<I>(input: I, current_cup: CupID) -> Self where I: Iterator<Item = CupID> {
-        let ids: Vec<CupID> = input.collect();
-        let mut cups: Vec<Cup> = (1..=ids.len()).map(|id| Cup { id, next: ptr::null_mut() }).collect();
+    fn new<I>(input: I, current: Cup) -> Self where I: Iterator<Item = Cup> {
+        let ids: Vec<Cup> = input.collect();
+        let last = ids.len()-1;
+        let mut cups: Vec<Cup> = (2..=ids.len()+1).collect();
+        cups[last] = 1;
 
-        for i in 0..cups.len()-1 {
-            cups[ids[i]-1].next = &mut cups[ids[i+1]-1] as *mut Cup;
+        for i in 1..ids.len() {
+            cups[ids[i-1]-1] = ids[i];
         }
-
-        unsafe {
-            let len = cups.len();
-            let mut last = &mut cups[ids[len-1]-1] as *mut Cup;
-            (*last).next = &mut cups[ids[0]-1] as *mut Cup;
-        }
-
-        let current = &mut cups[current_cup-1] as *mut Cup;
+        cups[ids[last]-1] = ids[0];
 
         Cups {
             length: cups.len(),
@@ -45,22 +33,28 @@ impl Cups {
         }
     }
 
-    fn prev_id(&self, cup: CupID) -> CupID {
-        if cup == 1 { self.length as CupID } else { cup - 1 }
-    }
-
-    fn from_str(input: &str, current_cup: CupID) -> Self {
+    fn from_str(input: &str, current_cup: Cup) -> Self {
         Cups::new(str_as_cup_ids(input), current_cup)
     }
 
-    fn labels(&self) -> Vec<CupID> {
+    fn next(&self, id: Cup) -> Cup {
+        self.cups[id-1]
+    }
+
+    fn set(&mut self, from: Cup, to: Cup) {
+        self.cups[from-1] = to;
+    }
+
+    fn prev_id(&self, cup: Cup) -> Cup {
+        if cup == 1 { self.length as Cup } else { cup - 1 }
+    }
+
+    fn labels(&self) -> Vec<Cup> {
         let mut labels = vec![];
-        unsafe {
-            let mut cup = self.cups[0].next as *const Cup;
-            for _ in 1..self.length {
-                labels.push((*cup).id);
-                cup = (*cup).next as *const Cup;
-            }
+        let mut cup = self.next(1);
+        for _ in 1..self.length {
+            labels.push(cup);
+            cup = self.next(cup);
         }
         labels
     }
@@ -70,35 +64,27 @@ impl Cups {
     }
 
     fn apply_move(&mut self) {
-        unsafe {
-            let curr = self.current;
-            let next1 = (*curr).next;
-            let next2 = (*next1).next;
-            let next3 = (*next2).next;
-            let next4 = (*next3).next;
+        let curr = self.current;
+        let next1 = self.next(curr);
+        let next2 = self.next(next1);
+        let next3 = self.next(next2);
+        let next4 = self.next(next3);
 
-            let mut dest_id = self.prev_id((*curr).id);
-            let mut dest;
-            loop {
-                dest = &mut self.cups[dest_id - 1];
-                if ptr::eq(dest, next1) || ptr::eq(dest, next2) || ptr::eq(dest, next3) {
-                    dest_id = self.prev_id(dest_id);
-                } else {
-                    break;
-                }
-            }
-
-            let dest_next1 = (*dest).next;
-
-            // remove next1..next3 from ring
-            (*curr).next = next4;
-
-            // reinsert after dest
-            (*dest).next = next1;
-            (*next3).next = dest_next1;
-
-            self.current = (*self.current).next;
+        let mut dest = self.prev_id(curr);
+        while dest == next1 || dest == next2 || dest == next3 {
+            dest = self.prev_id(dest);
         }
+
+        let dest_next1 = self.next(dest);
+
+        // remove next1..next3 from ring
+        self.set(curr, next4);
+
+        // reinsert after dest
+        self.set(dest, next1);
+        self.set(next3, dest_next1);
+
+        self.current = self.next(curr);
    }
 
     fn apply_n_moves(&mut self, count: usize) {
@@ -110,17 +96,17 @@ impl Cups {
 
 // -- problems
 
-fn part1(input: &str, start_cup: CupID) -> String {
+fn part1(input: &str, start_cup: Cup) -> String {
     let mut cups = Cups::from_str(input, start_cup);
     cups.apply_n_moves(100);
     cups.labels_as_str()
 }
 
-fn part2(input: &str, start_cup: CupID) -> CupID {
+fn part2(input: &str, start_cup: Cup) -> Cup {
     let mut cups = Cups::new(str_as_cup_ids(input).chain(10..=1_000_000), start_cup);
     cups.apply_n_moves(10_000_000);
 
-    let first_2: Vec<CupID> = cups.labels().iter().take(2).copied().collect();
+    let first_2: Vec<Cup> = cups.labels().iter().take(2).copied().collect();
     first_2[0] * first_2[1]
 }
 
@@ -137,6 +123,7 @@ mod tests {
     #[test]
     fn test_10_moves() {
         let mut cups = Cups::from_str("389125467", 3);
+        println!("{:?}", cups.labels());
         cups.apply_n_moves(10);
         assert_eq!(cups.labels(), vec![9, 2, 6, 5, 8, 3, 7, 4]);
     }
